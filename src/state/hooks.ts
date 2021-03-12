b746e751a38c8429f69a7be9b6db330bc9c84519
hachiojidev <hachiojidev@protonmail.com>
2021-03-12 09:39:42 +0900

    refactor: Round cards

--- src/state/hooks.ts
+++ src/state/hooks.ts
@@ -16,7 +16,7 @@ import {
   clear as clearToast,
   setBlock,
 } from './actions'
-import { State, Farm, Pool, Block, ProfileState, TeamsState, AchievementState, PriceState } from './types'
+import { State, Farm, Pool, Block, ProfileState, TeamsState, AchievementState, PriceState, RoundData } from './types'
 import { fetchProfile } from './profile'
 import { fetchTeam, fetchTeams } from './teams'
 import { fetchAchievements } from './achievements'
@@ -230,8 +230,13 @@ export const useInitializePredictions = () => {
 export const useGetRounds = () => {
   const rounds = useSelector((state: State) => state.predictions.rounds)
 
-  return useMemo(() => {
-    return Object.values(rounds).map(transformRoundResponse)
+  return useMemo<RoundData>(() => {
+    return Object.keys(rounds).reduce((accum, epoch) => {
+      return {
+        ...accum,
+        [epoch]: transformRoundResponse(rounds[epoch]),
+      }
+    }, {})
   }, [rounds])
 }
 
@@ -242,4 +247,14 @@ export const useGetCurrentEpoch = () => {
 export const useGetLiveRound = () => {
   const { currentEpoch, rounds } = useSelector((state: State) => state.predictions)
   return rounds[currentEpoch]
+}
+
+export const useGetPredictionsStatus = () => {
+  return useSelector((state: State) => state.predictions.status)
+}
+
+export const useGetCurrentRound = () => {
+  const currentEpoch = useGetCurrentEpoch()
+  const roundData = useSelector((state: State) => state.predictions.rounds)
+  return roundData[currentEpoch]
 }
