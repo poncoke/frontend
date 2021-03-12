eea8c7e7d389f362c1d5100f1bf8fbb7dcbdbf93
hachiojidev <hachiojidev@protonmail.com>
2021-03-12 16:22:08 +0900

    style(predictions): Header pane

--- src/state/hooks.ts
+++ src/state/hooks.ts
@@ -234,6 +234,10 @@ export const useBlock = (): Block => {
 }
 
 // Predictions
+export const useIsHistoryPaneOpen = () => {
+  return useSelector((state: State) => state.predictions.isHistoryPaneOpen)
+}
+
 export const useInitializePredictions = () => {
   const dispatch = useDispatch()
 
